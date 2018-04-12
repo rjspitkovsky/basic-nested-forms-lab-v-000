393de0aa4a939465506ac4476972c9441caf7218
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-12 04:03:59 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build
-    @recipe.ingredients.build
+    @recipe.ingredients.new
+    @recipe.ingredients.new
   end
 
   def create
